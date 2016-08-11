bd71e182db7fd2e45a486281b3fd3432b96894bb
Ignacio Rasche <nacho.rasche@gmail.com>
2016-08-11 16:41:50 +0200

    Added Procfile

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+
 var monthNames = [
     "January", "February", "March",
     "April", "May", "June",
@@ -50,6 +52,7 @@ function naturalize (date){
 	str += date.getFullYear()
 	return str
 }
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
