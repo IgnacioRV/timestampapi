02d8c4a05215aea3e43e1625ccb9dfe0f1af4f71
Ignacio Rasche <nacho.rasche@gmail.com>
2016-08-11 11:58:32 +0200

    Implemented the time processing API server

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var app = express();
+var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+];
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function getMonthName(month) {
+    return monthNames[month];
+};
+/*
+var bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({extended: false}))
+*/
+app.get('/:data', function (req, res) {
+  var data = parseInt(req.params.data)
+  var date = new Date(data*1000);
+  var isTS = date.getTime() > 0
+  var obj = {
+  	"unix": null,
+  	"natural": null
+  }
+  if (isTS){
+  	console.log("It's a unix timestamp")
+  	obj.unix = data
+  	obj.natural = naturalize(date)
+  }
+  else {
+  	console.log("NOT a unix timestamp")
+  	data = req.params.data
+  	console.log(data)
+  	dat = new Date (Date.parse(data))
+  	var isNT = date.getTime() > 0
+  	if (isNT){
+  	obj.unix = dat.getTime()
+  	obj.natural = naturalize(dat)
+  }
+}
+  res.send(obj)
 });
 
+function naturalize (date){
+	var str = ""
+	str += getMonthName(date.getMonth())
+	str += " "
+	str += date.getDate()
+	str += ", "
+	str += date.getFullYear()
+	return str
+}
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
